43e271234ce57afdbc7217d113087c9ae49fde54
S <s>
2024-11-23 03:12:58 +0400

    {"types":"C","runs":91}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -1,3 +1,4 @@
+
 /*
  * File: HangmanCanvas.java
  * ------------------------
@@ -7,7 +8,7 @@
 import acm.graphics.*;
 
 public class HangmanCanvas extends GCanvas {
-	
+
 	private static GLine body;
 	private static GLine leftHand;
 	private static GLine rightHand;
@@ -15,7 +16,7 @@ public class HangmanCanvas extends GCanvas {
 	private static GCompound leftLeg = new GCompound();
 	private static GCompound rightLeg = new GCompound();
 
-/** Resets the display so that only the scaffold appears */
+	/** Resets the display so that only the scaffold appears */
 	public void reset() {
 		drawBody();
 		drawHead();
@@ -23,79 +24,78 @@ public class HangmanCanvas extends GCanvas {
 		drawRightLeg();
 	}
 
-/**
- * Updates the word on the screen to correspond to the current
- * state of the game.  The argument string shows what letters have
- * been guessed so far; unguessed letters are indicated by hyphens.
- */
+	/**
+	 * Updates the word on the screen to correspond to the current state of the
+	 * game. The argument string shows what letters have been guessed so far;
+	 * unguessed letters are indicated by hyphens.
+	 */
 	public void displayWord(String word) {
 		/* You fill this in */
 	}
 
-/**
- * Updates the display to correspond to an incorrect guess by the
- * user.  Calling this method causes the next body part to appear
- * on the scaffold and adds the letter to the list of incorrect
- * guesses that appears at the bottom of the window.
- */
+	/**
+	 * Updates the display to correspond to an incorrect guess by the user.
+	 * Calling this method causes the next body part to appear on the scaffold
+	 * and adds the letter to the list of incorrect guesses that appears at the
+	 * bottom of the window.
+	 */
 	public void noteIncorrectGuess(char letter) {
 		/* You fill this in */
 	}
-	private void drawHead(){
+
+	private void drawHead() {
 		double headX = getWidth() / 2 - HEAD_RADIUS;
 		double headY = getHeight() / 2 - OFFSET_Y - BODY_LENGTH - HEAD_RADIUS * 2;
 		head = new GOval(HEAD_RADIUS * 2, HEAD_RADIUS * 2);
 		add(head, headX, headY);
 	}
-	private void drawBody(){
+
+	private void drawBody() {
 		double bodyX = getWidth() / 2;
 		double bodyY = getHeight() / 2 - OFFSET_Y - BODY_LENGTH;
 		body = new GLine(bodyX, bodyY, bodyX, bodyY + BODY_LENGTH);
 		add(body);
 	}
-	private void drawLeftLeg(){
+
+	private void drawLeftLeg() {
 		double hipX = getWidth() / 2 - HIP_WIDTH;
 		double hipY = getHeight() / 2 - OFFSET_Y;
-		GLine hip = new GLine(hipX, hipY, getWidth()/2, hipY);
-		
+		GLine hip = new GLine(hipX, hipY, getWidth() / 2, hipY);
+
 		double legX = hipX;
 		double legY = hipY;
-		GLine leg = new GLine(legX, legY, legX, legY+LEG_LENGTH);
-		
-		
-		
+		GLine leg = new GLine(legX, legY, legX, legY + LEG_LENGTH);
+
 		double footX = legX;
 		double footY = legY + LEG_LENGTH;
 		GLine foot = new GLine(footX, footY, footX - FOOT_LENGTH, footY);
-		
+
 		leftLeg.add(hip);
 		leftLeg.add(leg);
 		leftLeg.add(foot);
 		add(leftLeg);
 	}
-	
-	private void drawRightLeg(){
+
+	private void drawRightLeg() {
 		double hipX = getWidth() / 2 + HIP_WIDTH;
 		double hipY = getHeight() / 2 - OFFSET_Y;
-		GLine hip = new GLine(hipX, hipY, getWidth()/2, hipY);
-		
+		GLine hip = new GLine(hipX, hipY, getWidth() / 2, hipY);
+
 		double legX = hipX;
 		double legY = hipY;
-		GLine leg = new GLine(legX, legY, legX, legY+LEG_LENGTH);
-		
-		
-		
+		GLine leg = new GLine(legX, legY, legX, legY + LEG_LENGTH);
+
 		double footX = legX;
 		double footY = legY + LEG_LENGTH;
 		GLine foot = new GLine(footX, footY, footX + FOOT_LENGTH, footY);
-		
+
 		rightLeg.add(hip);
 		rightLeg.add(leg);
 		rightLeg.add(foot);
 		add(rightLeg);
 	}
 
-/* Constants for the simple version of the picture (in pixels) */
+	/* Constants for the simple version of the picture (in pixels) */
 	private static final int SCAFFOLD_HEIGHT = 360;
 	private static final int BEAM_LENGTH = 144;
 	private static final int ROPE_LENGTH = 18;
