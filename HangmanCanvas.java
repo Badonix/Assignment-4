282e17dbeeab77c5f90ee809294214fb24ed0766
S <s>
2024-11-23 03:10:44 +0400

    {"types":"C","runs":87}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -64,7 +64,7 @@ public class HangmanCanvas extends GCanvas {
 		
 		
 		double footX = legX;
-		double footY = legY + FOOT_LENGTH;
+		double footY = legY + LEG_LENGTH;
 		GLine foot = new GLine(footX, footY, footX - FOOT_LENGTH, footY);
 		
 		leftLeg.add(hip);
